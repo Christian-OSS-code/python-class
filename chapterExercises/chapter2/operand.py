72399d8d503b4aed3417d93e251fc5051a187ffb
christian <christianmary@gmail.com>
2024-10-19 18:28:51 +0100

    solution to program on operands

--- chapterExercises/chapter2/operand.py
+++ chapterExercises/chapter2/operand.py
@@ -1,12 +1,8 @@
-first = 27.5 + 2
-second = 27.5 - 2
-third = 27.5*2
-fourth = 27.5/2
-fifth = 27.5//2
-sixth = 27.5**2
-print(first)
-print(second)
-print(third)
-print(fourth)
-print(fifth)
-print(sixth)
+number1 = 2.75
+number2 = 2
+print(number1 + number2)
+print(number1 - number2)
+print(number1*number2)
+print(number1/number2)
+print(number1//number2)
+print(number1**number2)
