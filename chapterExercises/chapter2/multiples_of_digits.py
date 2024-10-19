24420b4f7d270f6d4630da86e9e98b28665afbb7
christian <christianmary@gmail.com>
2024-10-19 18:07:36 +0100

    solution to program on multiples of numbers

---
+++ chapterExercises/chapter2/multiples_of_digits.py
@@ -0,0 +1,12 @@
+number1 = 1024
+number2 = 4
+number3 = 2
+number4 = 10
+if (number1%number4 == 0):
+ print(number1, " is a multiple of ", number2)
+else:
+ print(number1, " is not a multiple of ", number2)
+if (number3%number4 == 0):
+ print(number3, " is a multiple of ", number4)
+else: 
+ print(number3, " is not a multiple of ", number4)
