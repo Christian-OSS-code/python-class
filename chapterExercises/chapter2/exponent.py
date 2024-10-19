0ebfd59b31ff2a08606684a10ee1cb04c7406f90
christian <christianmary@gmail.com>
2024-10-19 18:06:13 +0100

    solution on exponent limit of python

---
+++ chapterExercises/chapter2/exponent.py
@@ -0,0 +1,12 @@
+number = 5
+exponent_number1 = 100
+exponent_number2 = 1000
+exponent_number3 = 10000
+
+result1 = (number)**(exponent_number1)
+result2 = (number)**(exponent_number2)
+result3 = (number)**(exponent_number3)
+
+print(result1)
+print("\n",result2)
+print("\n",result3)
