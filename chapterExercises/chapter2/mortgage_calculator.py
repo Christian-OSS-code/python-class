9d6de9fc4190ebdae31577b87da96be597e3d9f5
christian <christianmary@gmail.com>
2024-10-21 09:32:46 +0100

    program on mortgage_calculator

--- chapterExercises/chapter2/mortgage_calculator.py
+++ chapterExercises/chapter2/mortgage_calculator.py
@@ -2,6 +2,6 @@
 principal = float(input("Enter the principal: "))
 rate = float(input("Enter the rate: "))
 number_of_years = int(input("Enter the year: "))
-monthly_payment = (principal)*(rate*(1 + rate)**(number_of_years))/((1 + rate)**(number_of_years) - 1)
+monthly_payment = (principal)*((rate/1200)*(1 + (rate/1200))**(number_of_years*12))/((1 + (rate/1200))**(number_of_years*12) - 1)
 print("The monthly payment is", monthly_payment)
  
