fb9102e4e237f5ddcc90a5b10a726b859c374cf5
Christian-OSS-code <christianmary2020@gmail.com>
2024-12-02 16:59:41 +0100

    third commit phase gate

---
+++ PhaseGateOne/dayoftheweek.py
@@ -0,0 +1,314 @@
+day_of_the_week = int(input("Enter today's day:\n0Sunday\n1.Monday\n2.Tuesday\3.Wednesday\n4.Thursday\5.Friday\6.Saturday"))
+
+match day(day_of_the_week):
+    case 0: 
+        print("Today is Sunday")
+        find_out = int(input("Enter number to check future date: "))
+            
+        future_day = day_of_the_week + find_out
+            
+        match futher_day (future_day):
+                case 1: 
+                    print("Today is Monday")
+
+                    break
+    
+                case 2:
+                    print("Today is Tuesday")
+
+                    break
+
+                case 3:
+                    print("Today is Wednesday")
+
+                    break
+
+                case 4:
+                    print("Today is Thursday")
+                    break
+                case 5:
+
+                    print("Today is Friday")
+                
+                    break
+
+                case 6:
+                    print("Today is Saturday")
+        
+                    break
+    
+    case 1: 
+        print("Today is Monday")
+
+       
+        find_out = int(input("Enter number to check future date: "))
+            
+        future_day = day_of_the_week + find_out
+            
+        match futher_day (future_day):
+               case 1: 
+                    print("Today is Monday")
+
+                    break
+    
+               case 2:
+                    print("Today is Tuesday")
+
+                    break
+
+               case 3:
+                    print("Today is Wednesday")
+
+                    break
+
+               case 4:
+                    print("Today is Thursday")
+                    break
+               case 5:
+
+                    print("Today is Friday")
+                
+                    break
+
+               case 6:
+                    print("Today is Saturday")
+        
+                    break
+    
+    case 2:
+        print("Today is Tuesday")
+
+        break
+
+        find_out = int(input("Enter number to check future date: "))
+            
+        future_day = day_of_the_week + find_out
+            
+        match futher_day (future_day):
+                case 1: 
+                    print("Today is Monday")
+
+                    break
+    
+                case 2:
+                    print("Today is Tuesday")
+
+                    break
+
+                case 3:
+                    print("Today is Wednesday")
+
+                    break
+
+                case 4:
+                    print("Today is Thursday")
+                    break
+                case 5:
+
+                    print("Today is Friday")
+                
+                    break
+
+                case 6:
+                    print("Today is Saturday")
+        
+                    break
+
+    case 3:
+        print("Today is Tuesday")
+
+
+        find_out = int(input("Enter number to check future date: "))
+            
+        future_day = day_of_the_week + find_out
+            
+        match futher_day (future_day):
+                case 1: 
+                    print("Today is Monday")
+
+                    break
+    
+                case 2:
+                    print("Today is Tuesday")
+
+                    break
+
+                case 3:
+                    print("Today is Wednesday")
+
+                    break
+
+                case 4:
+                    print("Today is Thursday")
+                    break
+                case 5:
+
+                    print("Today is Friday")
+                
+                    break
+
+                case 6:
+                    print("Today is Saturday")
+        
+                    break
+
+    case 4:
+        print("Today is Wednesday")
+        find_out = int(input("Enter number to check future date: "))
+            
+        future_day = day_of_the_week + find_out
+            
+        match futher_day (future_day):
+                case 1: 
+                    print("Today is Monday")
+
+                    break
+    
+                case 2:
+                    print("Today is Tuesday")
+
+                    break
+
+                case 3:
+                    print("Today is Wednesday")
+
+                    break
+
+                case 4:
+                    print("Today is Thursday")
+                    break
+                case 5:
+
+                    print("Today is Friday")
+                
+                    break
+
+                case 6:
+                    print("Today is Saturday")
+        
+                    break
+    
+    
+
+    case 5:
+        print("Today is Thursday")
+        find_out = int(input("Enter number to check future date: "))
+            
+        future_day = day_of_the_week + find_out
+            
+        match futher_day (future_day):
+                case 1: 
+                    print("Today is Monday")
+
+                    break
+    
+                case 2:
+                    print("Today is Tuesday")
+
+                    break
+
+                case 3:
+                    print("Today is Wednesday")
+
+                    break
+
+                case 4:
+                    print("Today is Thursday")
+                    break
+                case 5:
+
+                    print("Today is Friday")
+                
+                    break
+
+                case 6:
+                    print("Today is Saturday")
+        
+         
+
+                    break
+
+    case 5:
+
+        print("Today is Friday")
+        find_out = int(input("Enter number to check future date: "))
+            
+        future_day = day_of_the_week + find_out
+            
+        match futher_day (future_day):
+                case 1: 
+                    print("Today is Monday")
+
+                    break
+    
+                case 2:
+                    print("Today is Tuesday")
+
+                    break
+
+                case 3:
+                    print("Today is Wednesday")
+
+                    break
+
+                case 4:
+                    print("Today is Thursday")
+                    break
+                case 5:
+
+                    print("Today is Friday")
+                
+                    break
+
+                case 6:
+                    print("Today is Saturday")
+        
+                    break
+           
+
+    case 6:
+    
+        print("Today is Saturday")
+
+        find_out = int(input("Enter number to check future date: "))
+            
+        future_day = day_of_the_week + find_out
+            
+        match futher_day (future_day):
+                case 1: 
+                    print("Today is Monday")
+
+                    break
+    
+                case 2:
+                    print("Today is Tuesday")
+
+                    break
+
+                case 3:
+                    print("Today is Wednesday")
+
+                    break
+
+                case 4:
+                    print("Today is Thursday")
+                    break
+                case 5:
+
+                    print("Today is Friday")
+                
+                    break
+
+                case 6:
+                    print("Today is Saturday")
+        
+            
+
+
+
+
+
+
+
+
+
